9d01d6a1dfd91259a5dccfcddd05685ee29d6e7e
Nova-B <46260481+Nova-B@users.noreply.github.com>
2022-10-13 11:19:29 +0900

    원형으로 음성 데이터 반영한 큐브 움직임까지 완성
    https://youtu.be/Ri1uNPNlaVs

---
+++ VR_BeatSaber_project/Assets/Scripts/Instantiate512Cubes.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Instantiate512Cubes : MonoBehaviour
+{
+    public GameObject sampleCubePrefab;
+
+    GameObject[] sampleCube = new GameObject[512];
+    public float masScale;
+    // Start is called before the first frame update
+    void Start()
+    {
+        for(int i = 0; i <512; i++)
+        {
+            GameObject instSampleCube = (GameObject)Instantiate(sampleCubePrefab);
+            instSampleCube.transform.position = this.transform.position;
+            instSampleCube.transform.parent = this.transform;
+            instSampleCube.name = "SampleCube" + i;
+            this.transform.eulerAngles = new Vector3(0, -(360 / 512f) * i, 0);
+            instSampleCube.transform.position = Vector3.forward * 100;
+            sampleCube[i] = instSampleCube;
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        for(int i = 0; i < 512; i++)
+        {
+            if(sampleCube != null)
+            {
+                sampleCube[i].transform.localScale = new Vector3(1, (AudioPeer.samples[i] * masScale) + 2, 1);
+            }
+        }
+    }
+}
